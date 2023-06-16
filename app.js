19403c2ef3054991394d627cac2f463aa0d005f3
Ait hammou brahim hicham <hichambrahim1999@outlook.com>
2023-06-16 10:35:58 +0100

    learn new thing

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const mySliderImages =  Array.from(document.querySelectorAll("main img"));
+const mySliderImages = Array.from(document.querySelectorAll("main img"));
 const sliderCountInfo = document.querySelector(".slide-info");
 const nextButton = document.querySelector(".next");
 const previousButton = document.querySelector(".previous");
@@ -12,95 +12,78 @@ createDomElement();
 
 const bullets = Array.from(document.querySelectorAll(".bullets span"));
 
-nextButton.addEventListener("click" , handleNextButton);
+nextButton.addEventListener("click", handleNextButton);
 
-previousButton.addEventListener("click" , handlePreviousButton);
+previousButton.addEventListener("click", handlePreviousButton);
 
 bullets.forEach((bullet) => {
-        bullet.addEventListener("click" , () => {
-           handleBullets(bullet);
-        });
-})
+  bullet.addEventListener("click", () => {
+    handleBullets(bullet);
+  });
+});
 
 mainFuncHandling();
 
 // functions
 
 function handleNextButton() {
-    if(nextButton.classList.contains("disable")) {
-        return 0;
-    }
-    else {
-        count++;
-        mainFuncHandling();
-    }
+  if (nextButton.classList.contains("disable")) {
+    return 0;
+  } else {
+    count++;
+    mainFuncHandling();
+  }
 }
 
 function handlePreviousButton() {
-    if(previousButton.classList.contains("disable")) {
-        return 0;
-    }
-    else {
-        count--;
-        mainFuncHandling();
-    }
-
+  if (previousButton.classList.contains("disable")) {
+    return 0;
+  } else {
+    count--;
+    mainFuncHandling();
+  }
 }
 
-
 function handleBullets(bullet) {
-    count = parseInt(bullet.dataset.id);
-    mainFuncHandling();
+  count = parseInt(bullet.dataset.id);
+  mainFuncHandling();
 }
 
 function mainFuncHandling() {
-        sliderCountInfo.innerHTML = `Slide #${count} of ${silderCount}`;
+  sliderCountInfo.innerHTML = `Slide #${count} of ${silderCount}`;
 
-        removeAllClasses(mySliderImages);
-        removeAllClasses(bullets);
+  removeAllClasses(mySliderImages);
+  removeAllClasses(bullets);
 
-        mySliderImages[count - 1].classList.add("active");
-        bullets[count - 1].classList.add("active");
-        checkStatus();
+  mySliderImages[count - 1].classList.add("active");
+  bullets[count - 1].classList.add("active");
+  checkStatus();
 }
-
-
 
 function createDomElement() {
-    let output = "";
-    for (let i = 0; i < silderCount ; i++) {
-        output += `<span data-id="${i+1}">${i+1}</span>`; 
-    }
-    bulletsContainer.innerHTML = output;
+  let output = "";
+  for (let i = 0; i < silderCount; i++) {
+    output += `<span data-id="${i + 1}">${i + 1}</span>`;
+  }
+  bulletsContainer.innerHTML = output;
 }
-
-
-
-
-
-
 
 function checkStatus() {
-    if( count === 1) {
-        previousButton.classList.add("disable");
-    }
-    else {
-        previousButton.classList.remove("disable");
-    }
-    if( count === silderCount) {
-        nextButton.classList.add("disable");
-    }
-    else {
-        nextButton.classList.remove("disable");
-    }
-    
+  if (count === 1) {
+    previousButton.classList.add("disable");
+  } else {
+    previousButton.classList.remove("disable");
+  }
+  if (count === silderCount) {
+    nextButton.classList.add("disable");
+  } else {
+    nextButton.classList.remove("disable");
+  }
 }
 function removeAllClasses(elmenets) {
-    elmenets.forEach(element => {
-        element.classList.remove("active");
-    });
+  elmenets.forEach((element) => {
+    element.classList.remove("active");
+  });
 }
 
-
-
-
+console.log("learn how to use git and github");
